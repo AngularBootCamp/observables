924200197226281666ebfaf0a152a8f196c38bb6
Lance Finney <lance.finney@oasisdigital.com>
2018-03-09 23:03:32 -0600

    curriculum update

--- src/app/employee-loader.service.ts
+++ src/app/employee-loader.service.ts
@@ -5,12 +5,6 @@ import { map } from 'rxjs/operators';
 
 const API_URL = 'https://api.angularbootcamp.com';
 
-// Or connect directly to the API server (local development only)
-// const API_URL = 'http://localhost:8085';
-
-// Or connect to the hosted demo API (works from local, Cloud 9, StackBlitz):
-// const API_URL = 'https://api.angularbootcamp.com';
-
 export interface Employee {
   first_name: string;
   last_name: string;
